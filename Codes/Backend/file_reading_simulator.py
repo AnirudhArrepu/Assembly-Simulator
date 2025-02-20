35485b4bfed48b60c7f49f40d3c8929b0d837346
AnirudhArrepu <anirudharrepu@gmail.com>
2025-02-20 22:20:29 +0530

    added file reading

--- Codes/Backend/file_reading_simulator.py
+++ Codes/Backend/file_reading_simulator.py
@@ -2,8 +2,8 @@ from concurrent.futures import ThreadPoolExecutor
 import matplotlib.pyplot as plt
 import numpy as np
 
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+# from flask import Flask, request, jsonify, render_template
+# from flask_cors import CORS
 
 class Core:
     def __init__(self, coreid, memory):
@@ -230,36 +230,36 @@ class Simulator:
             
             self.clock += 1
 
-program = '''
-.data
-arr: .word 0x144 0x3 0x9 0x8 0x1 0x100
+# program = '''
+# .data
+# arr: .word 0x144 0x3 0x9 0x8 0x1 0x100
 
-.text
-la x3 arr
-addi x4 x0 6
-addi x7 x0 0
-outer_loop: addi x11 x4 -1
-beq x7 x11 outer_exit
-addi x10 x3 0
-addi x8 x0 0
-inner_loop: addi x12 x4 0
-sub x12 x12 x7
-addi x12 x12 -1
-beq x8 x12 inner_exit
-lw x5 0(x10)
-lw x6 4(x10)
-slt x11 x6 x5
-beq x11 x0 no_swap
-sw x5 4(x10)
-sw x6 0(x10)
-no_swap: addi x10 x10 4
-addi x8 x8 1
-j inner_loop
-inner_exit: addi x7 x7 1
-j outer_loop
-outer_exit: j exit
-exit: addi x0 x0 0
-'''
+# .text
+# la x3 arr
+# addi x4 x0 6
+# addi x7 x0 0
+# outer_loop: addi x11 x4 -1
+# beq x7 x11 outer_exit
+# addi x10 x3 0
+# addi x8 x0 0
+# inner_loop: addi x12 x4 0
+# sub x12 x12 x7
+# addi x12 x12 -1
+# beq x8 x12 inner_exit
+# lw x5 0(x10)
+# lw x6 4(x10)
+# slt x11 x6 x5
+# beq x11 x0 no_swap
+# sw x5 4(x10)
+# sw x6 0(x10)
+# no_swap: addi x10 x10 4
+# addi x8 x8 1
+# j inner_loop
+# inner_exit: addi x7 x7 1
+# j outer_loop
+# outer_exit: j exit
+# exit: addi x0 x0 0
+# '''
 
 
 def preprocess(program):
@@ -296,37 +296,52 @@ def main(program):
 
     return sim
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/')
-def index():
-    return render_template('gui.html')
-
-@app.route('/simulate', methods=['POST'])
-def simulate():
-    data = request.json
-    program = data['program']
-    print(program)
+with open('./assembly.asm', 'r') as file:
+    program = file.read()
     sim = main(program)
 
-    memories = sim.memory.printMemory()
+    print(program)
 
-    return jsonify({
-        'core0': sim.cores[0].registers,
-        'core1': sim.cores[1].registers,
-        'core2': sim.cores[2].registers,
-        'core3': sim.cores[3].registers,
-        'clock': sim.clock,
-        'memory': memories,
-        'memory1': memories[0],
-        'memory2': memories[1],
-        'memory3': memories[2],
-        'memory4': memories[3],
-    })
+    print(sim.cores[0].registers)
+    print(sim.cores[1].registers)
+    print(sim.cores[2].registers)
+    print(sim.cores[3].registers)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    print(f"number of clock cycles: {sim.clock}")
+
+    sim.memory.printMemory()
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('gui.html')
+
+# @app.route('/simulate', methods=['POST'])
+# def simulate():
+#     data = request.json
+#     program = data['program']
+#     print(program)
+#     sim = main(program)
+
+#     memories = sim.memory.printMemory()
+
+#     return jsonify({
+#         'core0': sim.cores[0].registers,
+#         'core1': sim.cores[1].registers,
+#         'core2': sim.cores[2].registers,
+#         'core3': sim.cores[3].registers,
+#         'clock': sim.clock,
+#         'memory': memories,
+#         'memory1': memories[0],
+#         'memory2': memories[1],
+#         'memory3': memories[2],
+#         'memory4': memories[3],
+#     })
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 # sim = Simulator()
